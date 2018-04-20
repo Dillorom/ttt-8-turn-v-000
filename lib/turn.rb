f404047cdf885a173d75b4371b51615ea5d8a7b8
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-04-20 04:11:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
   puts "#{board[0]} | #{board[2]} | #{board[2]}"
@@ -8,15 +7,29 @@ def display_board(board)
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
 
-def input_to_index(input)
-  input.to_i + 1
-end
 
 def valid_move?(board, index)
   if board[index] && position_taken?(board, index) == false
     return true
   end
 end
+
+
+def position_taken?(board, index)
+  if board[index] ==" " || board[index] == "" || board[index] == nil
+    return false
+
+  else
+    return true
+  end
+end
+
+
+def input_to_index(input)
+  input.to_i + 1
+end
+
+
 
 def move(board, index, character = "X")
   board(input) = character
